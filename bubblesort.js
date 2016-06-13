943b4b72e72b44203120cd9111d0db60bde7f4a0
Sarah Welz Geselowitz <sarahgeselowitz@gmail.com>
2016-06-13 12:37:51 -0400

    final

--- bubblesort.js
+++ bubblesort.js
@@ -1,25 +1,30 @@
 function bubbleSort(array){
 	var copy;
-	//var swapped;
 	var counter;
+	var end=array.length-1;
 	while(true){
-		//swapped=false;
 		counter=0;
-		for(var i=0;i<array.length-1;i++){
-			if(array[i]>array[i+1]){
+		for(var i=0;i<end;i++){
+			if(compare(i,i+1,array)){
 				copy=array[i];
 				array[i]=array[i+1];
 				array[i+1]=copy;
-				//swapped=true;
 				counter++;
-			}	
+			}
+			//console.log('counter',counter);
 		}
+		end--;
 		//console.log(array);
-		//if(swapped===false){
-		if(counter<=1){
+		if(counter===0){
 			return array;
 		}
 	}
 }
+
+function compare(a,b,array){
+	return array[a]>array[b];
+}
+
+
 
 
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,11 +1,43 @@
 describe('Bubble Sort', function(){
+
   it('handles an empty array', function(){
     expect( bubbleSort([]) ).toEqual( [] );
   });
-  it('handles an empty array', function(){
+  it('handles an array of size1', function(){
     expect( bubbleSort([1]) ).toEqual( [1] );
   });
-  it('handles an empty array', function(){
+  it('handles an array', function(){
     expect( bubbleSort([1,6,3,4566,34,2,0,-2]) ).toEqual([-2,0,1,2,3,6,34,4566]);
   });
+  it('handles an array2', function(){
+    expect( bubbleSort([4,6,5,1]) ).toEqual([1,4,5,6]);
+  });
+  /*
+  it('compares the expected number of times', function(){
+  	spyOn(window,'compare').and.callThrough();
+  	var sorted=bubbleSort([4,6,5,1]);
+  	//expect(sorted).toEqual([1,4,5,6]);
+    expect(compare.calls.count()).toEqual(10);
+  });*/ //not sure why we're getting 6 and not 10
+
+  function numerically (a, b) { return a - b; }
+
+  function generateArray(size, lower, upper) {
+  var randomArray = [];
+  while (size--) {
+    var randomNum = Math.floor(lower + Math.random() * upper);
+    randomArray.push(randomNum);
+  }
+  return randomArray;
+}
+for (var i = 2; i < 103; i += 20) {
+    it('sorts an array of ' + i + ' random items', function(){
+      var arr = generateArray(i, 0, 100);
+      var sorted = arr.slice(0).sort(numerically);
+      expect( bubbleSort(arr) ).toEqual( sorted );
+    });
+  }
+
 });
+
+
